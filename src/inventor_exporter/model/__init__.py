d906427757bc5136fb7b02bb4dfdf20190e13786
Jonathan <jon-cama@uniandes.edu.co>
2026-01-19 14:05:25 -0500

    feat(02-01): export model package public API
    - Re-exports Transform, Material, Inertia from package root
    - Follows same pattern as core/__init__.py
    - Single import path for all IR dataclasses

--- src/inventor_exporter/model/__init__.py
+++ src/inventor_exporter/model/__init__.py
@@ -1,1 +1,21 @@
-"""Model package - intermediate representation dataclasses."""
+"""Model package - intermediate representation dataclasses.
+
+This package provides the data model for the intermediate representation (IR)
+that captures assembly structure extracted from Inventor. All format writers
+consume this common data model.
+
+Foundation dataclasses:
+    Transform: 6-DOF pose (position + rotation)
+    Material: Physical material properties (density, etc.)
+    Inertia: Mass properties with tensor transformation methods
+"""
+
+from inventor_exporter.model.transform import Transform
+from inventor_exporter.model.material import Material
+from inventor_exporter.model.inertia import Inertia
+
+__all__ = [
+    "Transform",
+    "Material",
+    "Inertia",
+]
